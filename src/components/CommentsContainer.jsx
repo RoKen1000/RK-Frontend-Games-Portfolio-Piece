0bc5e19aef8becaa2c6fc7f01c30651af5f4d0a1
RoKen1000 <rosskennedy993@outlook.com>
2023-04-19 09:58:52 +0100

    Add in conditional rendering to display when comments are loading on slower connections and add conditional rendering to display a message when no comments are posted on a review.

--- src/components/CommentsContainer.jsx
+++ src/components/CommentsContainer.jsx
@@ -6,13 +6,20 @@ import "../styles/CommentContainer.css"
 export const CommentsContainer = ({review_id, user}) => {
 
     const [comments, setComments] = useState([])
+    const [commentsLoading, setCommentsLoading] = useState(false)
 
     useEffect(() => {
+        setCommentsLoading(true)
         fetchReviewComments(review_id)
         .then((retrievedComments) => {
+            setCommentsLoading(false)
             setComments(retrievedComments)
         })
     }, [])
+
+    if(commentsLoading) return <h1 className="comments-loading-message">Comments Loading...</h1>
+
+    if(comments.length === 0) return <h1 className="no-comments-message">No Comments</h1>
 
     return(
         <div className="comments-container">
