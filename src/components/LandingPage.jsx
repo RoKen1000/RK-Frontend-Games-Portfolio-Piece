75184cbc7f72087248c393f65086cb25b74e6384
RoKen1000 <rosskennedy993@outlook.com>
2023-04-23 13:46:36 +0100

    Create landing page and its CSS. Choose font for entire app.

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,22 @@
+import logo from "../images/logo.png"
+import "../styles/LandingPage.css"
+import { LoggedInUser } from "./LoggedInUser"
+import { Link} from "react-router-dom"
+
+export const LandingPage = ({user}) => {
+
+    return(
+        <main>
+            <h1 className="landing-page-welcome">Welcome to NC Games!</h1>
+            <h2 className="landing-page-welcome">The Central Hub for All Board-Game Reviews</h2>
+            <div className="landing-page-container">
+                <img src={logo} alt="website-logo" className="landing-page-logo"/>
+                <div className="landing-page-links">
+                    <Link to="/reviews">All Reviews</Link>
+                    <Link to="/categories">Categories</Link>
+                </div>
+                <LoggedInUser user={user}/>
+            </div>
+        </main>
+    )
+}
