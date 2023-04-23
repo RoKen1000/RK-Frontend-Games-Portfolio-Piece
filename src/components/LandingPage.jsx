1c6a039abf92d8dacc7ee3be2eaac50d98bdc7e1
RoKen1000 <rosskennedy993@outlook.com>
2023-04-23 15:33:57 +0100

    Change index.html from create-react-app template. Give site title, description and favicon.

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,7 +8,7 @@ export const LandingPage = ({user}) => {
     return(
         <main>
             <h1 className="landing-page-welcome">Welcome to NC Games!</h1>
-            <h2 className="landing-page-welcome">The Central Hub for All Board-Game Reviews</h2>
+            <h2 className="landing-page-welcome">The Central Hub for All Board Game Reviews</h2>
             <div className="landing-page-container">
                 <img src={logo} alt="website-logo" className="landing-page-logo"/>
                 <div className="landing-page-links">
