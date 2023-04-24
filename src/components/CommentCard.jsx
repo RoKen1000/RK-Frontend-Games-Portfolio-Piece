2204a15bdce06a36b3c8cb4e09bc6c609d27c3b9
RoKen1000 <rosskennedy993@outlook.com>
2023-04-24 20:39:16 +0100

    Added better support for Galaxy Fold mobile device to avoid review cards and posted comments exceeding the viewport.

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -10,7 +10,7 @@ export const CommentCard = ({votes, author, body, comment_date, review_id, user,
 
     return(
         <div className="comment-card">
-            <h1><strong>Posted By: {author}</strong></h1>
+            <h1 className="comment-posted-by"><strong>Posted By: {author}</strong></h1>
             <p><strong>Posted:</strong> {comment_date.slice(0, 10)}</p>
             <p className="comment-body">{body}</p>
             <p><strong>Votes: {votes}</strong></p>
