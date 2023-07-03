de782a4cfeee22a171b30f4bda582bcdbb22212e
RoKen1000 <rosskennedy993@outlook.com>
2023-07-03 14:13:33 +0100

    Added in Footer in all pages past the langing page. Add in required CSS and text content.

--- src/components/CategoriesContainer.jsx
+++ src/components/CategoriesContainer.jsx
@@ -3,14 +3,14 @@ import { fetchCategories } from "../api"
 import { CategoryCard } from "./CategoryCard"
 import {v4 as uuidv4} from "uuid"
 import "../styles/CategoriesContainer.css"
+import { Footer } from "./Footer"
 
 export const CategoriesContainer = () => {
 
     const [categories, setCategories] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        setIsLoading(true)
         fetchCategories()
         .then((retrievedCategories) => {
             setIsLoading(false)
@@ -21,17 +21,20 @@ export const CategoriesContainer = () => {
     if(isLoading) return <h1 className="loading-statement">Loading...</h1>
 
     return(
-        <main>
-            <h1 id="category-instruction">View reviews by category here</h1>
-            <div className="categories-container">
-                {categories.map((category) => {
-                    return <CategoryCard
-                        key={uuidv4()}
-                        category={category.slug}
-                        description={category.description}
-                    />
-                })}
-            </div>
-        </main>
+        <div>
+            <main>
+                <h1 id="category-instruction">View reviews by category here</h1>
+                <div className="categories-container">
+                    {categories.map((category) => {
+                        return <CategoryCard
+                            key={uuidv4()}
+                            category={category.slug}
+                            description={category.description}
+                        />
+                    })}
+                </div>
+            </main>
+            {isLoading ? null : <Footer/>}
+        </div>
     )
 }
--- src/components/FullReview.jsx
+++ src/components/FullReview.jsx
@@ -5,12 +5,13 @@ import "../styles/FullReview.css"
 import {CommentsContainer} from "./CommentsContainer"
 import { LikeButtons } from "./LikeButtons"
 import { ErrorComponent } from "./ErrorComponent"
+import { Footer } from "./Footer"
 
 export const FullReview = ({user}) => {
     const {review_id} = useParams()
 
     const [fullReview, setFullReview] = useState({})
-    const [reviewLoading, setReviewLoading] = useState(false)
+    const [reviewLoading, setReviewLoading] = useState(true)
     const [votes, setVotes] = useState(0)
     const [error, setError] = useState(null)
     
@@ -33,24 +34,27 @@ export const FullReview = ({user}) => {
     if(error) return <ErrorComponent error={error}/>
 
     return(
-        <article>
-            <section className="full-review-container">
-                <h1 className="full-review-title">{fullReview.title}</h1>
-                <h2 className="full-review-designer">Designed by {fullReview.designer}</h2>
-                <h2 className="full-review-category">Category: {fullReview.category}</h2>
-                <img className="full-review-image" src={fullReview.review_img_url} alt="sample-of-board-game-in-action"/>
-                <h2 className="full-review-info"><strong>By: {fullReview.owner}, Created At: {fullReview.created_at}</strong></h2>
-                <div className="full-review-body-container">
-                    <p className="full-review-body">{fullReview.review_body}</p>
-                </div>
-                <h2 className="full-review-vote-count">Votes: {votes}</h2>
-                <div className="full-review-button-container">
-                    <LikeButtons setVotes={setVotes} votes={votes} review_id={review_id} originalVoteCount={fullReview.votes}/>
-                </div>
-            </section>
-            <section>
-                <CommentsContainer review_id={review_id} user={user}/>
-            </section>
-        </article>
+        <div>
+            <article>
+                <section className="full-review-container">
+                    <h1 className="full-review-title">{fullReview.title}</h1>
+                    <h2 className="full-review-designer">Designed by {fullReview.designer}</h2>
+                    <h2 className="full-review-category">Category: {fullReview.category}</h2>
+                    <img className="full-review-image" src={fullReview.review_img_url} alt="sample-of-board-game-in-action"/>
+                    <h2 className="full-review-info"><strong>By: {fullReview.owner}, Created At: {fullReview.created_at}</strong></h2>
+                    <div className="full-review-body-container">
+                        <p className="full-review-body">{fullReview.review_body}</p>
+                    </div>
+                    <h2 className="full-review-vote-count">Votes: {votes}</h2>
+                    <div className="full-review-button-container">
+                        <LikeButtons setVotes={setVotes} votes={votes} review_id={review_id} originalVoteCount={fullReview.votes}/>
+                    </div>
+                </section>
+                <section>
+                    <CommentsContainer review_id={review_id} user={user}/>
+                </section>
+            </article>
+            {reviewLoading ? null : <Footer/>}
+        </div>
     )
 }
